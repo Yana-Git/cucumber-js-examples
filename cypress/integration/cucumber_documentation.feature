0d08a4b7768cfa8faf5ce5dd51ea3b48bf964e51
Wang Yao <yaowang274@gmail.com>
2021-02-02 18:32:53 +1300

    write homework

---
+++ cypress/integration/cucumber_documentation.feature
@@ -0,0 +1,17 @@
+Feature: Cucumber documentation
+    As a user of cucumber.js
+    I want to have documentation on cucumber
+    So I can write better applications
+
+
+    Scenario: Usage documentation
+        Given I am on the cucumber.js GitHub repository
+        When I go to the README file
+        Then I should see a "Usage" section
+
+
+    Scenario: Status badges
+        Given I am on the cucumber.js GitHub repository
+        When I go to the README file
+        Then I should see a "Dependencies" badge
+        And I can see a "Build" badge
---
+++ cypress/integration/cucumber_documentation/cucumber_documentation.js
@@ -0,0 +1,28 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given("I am on the cucumber.js GitHub repository", () => {
+  cy.visit("https://github.com/cucumber/cucumber-js");
+});
+
+When("I go to the README file", () => {
+  cy.visit("https://github.com/cucumber/cucumber-js/blob/master/README.md");
+});
+
+Then("I should see a {string} section", (title) => {
+  cy.get("a")
+    .find('[href="#' + title.toLowerCase() + '"]')
+    .should("not.exist");
+  cy.log("The " + title + " section was not found");
+});
+
+Then("I should see a {string} badge", (badge) => {
+  cy.get('[alt="' + badge + '"] ').should("exist");
+});
+
+Then("I can see a {string} badge", (badge) => {
+  cy.get(
+    '[src="https://github.com/cucumber/cucumber-js/workflows/' +
+      badge +
+      '/badge.svg"] '
+  ).should("exist");
+});
